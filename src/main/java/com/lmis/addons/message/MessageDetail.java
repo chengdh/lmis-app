26458f1b3b40fd9884403179f63d819fffbc4179
chengdh <cheng.donghui@gmail.com>
2014-08-28 14:25:15 +0800

    消息推送功能已完成

--- src/main/java/com/lmis/addons/message/MessageDetail.java
+++ src/main/java/com/lmis/addons/message/MessageDetail.java
@@ -1,8 +1,8 @@
 package com.lmis.addons.message;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +10,15 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import com.lmis.LmisArguments;
 import com.lmis.R;
 import com.lmis.orm.LmisDataRow;
+import com.lmis.orm.LmisHelper;
+import com.lmis.orm.LmisValues;
 import com.lmis.support.BaseFragment;
 import com.lmis.util.drawer.DrawerItem;
 
+import java.util.Date;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -42,6 +46,8 @@ public class MessageDetail extends BaseFragment {
     int mMessageId = -1;
 
     View mView = null;
+
+    MessageViewCountUpdater mUpdater = null;
 
     @Override
     public Object databaseHelper(Context context) {
@@ -72,13 +78,56 @@ public class MessageDetail extends BaseFragment {
             LmisDataRow row = db.select(mMessageId);
             mTxvTitle.setText(row.getString("title"));
             mTxvOrgName.setText(row.getString("org_name"));
-            mTxvPublishDate.setText(row.getString("publish_date").substring(0,10));
+            mTxvPublishDate.setText(row.getString("publish_date").substring(0, 10));
             String body = row.getString("body");
             //FIXME 此处参考了
             //http://wj495175289.blog.163.com/blog/static/1620826662012364512840/
             //对webview处理乱码的方法
             mWvBody.loadData(body, "text/html;charset=UTF-8", null);
 
+            //更新查看次数
+            updateViewCount();
+
         }
+    }
+
+    /**
+     * 更新查看次数.
+     */
+    private void updateViewCount() {
+        if (mUpdater != null) {
+            mUpdater.cancel(true);
+            mUpdater = null;
+        }
+        mUpdater = new MessageViewCountUpdater();
+        mUpdater.execute((Void) null);
+    }
+
+    private class MessageViewCountUpdater extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... arg0) {
+            Log.d(TAG, "MessageViewCountUpdater#doInBackground");
+            MessageDB db = (MessageDB) databaseHelper(scope.context());
+            LmisHelper helper = db.getLmisInstance();
+            LmisArguments arguments = new LmisArguments();
+            int currentUserId = scope.currentUser().getUser_id();
+            arguments.add(mMessageId);
+            arguments.add(currentUserId);
+
+            helper.call_kw("update_view_count", arguments);
+            LmisValues updateValues = new LmisValues();
+            updateValues.put("processed", true);
+            updateValues.put("process_datetime", new Date());
+            db.update(updateValues, mMessageId);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            Log.d(TAG, "MessageViewCountUpdater#onPostExecute");
+            mUpdater = null;
+        }
+
     }
 }
